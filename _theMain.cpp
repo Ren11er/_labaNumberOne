4371ef9de7573856d982f847149987e0f3c2d0fc
Renner <@DESKTOP-4LNKPB1>
2024-11-01 22:45:54 +0500

    Ren0.1

---
+++ _theMain.cpp
@@ -0,0 +1,31 @@
+﻿#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int _Menu(int vbr) {
+
+    cout << "=========================" << endl;
+
+    if (vbr == 1) {
+        cout << "1) Задание 1!" << endl;
+        cout << "1) Задание 2!" << endl;
+    }
+
+    if (vbr == 2) {
+
+    }
+
+    cout << "=========================" << endl;
+
+    cin >> vbr;
+    return vbr;
+}
+
+
+
+int main()
+{
+    int menu = _Menu(1);
+    system("cls");
+}
